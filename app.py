c1dc332eae19606c1ca473c195b19938082d2c02
carbuncle123 <ca.a1mandine.t@gmail.com>
2019-12-14 22:32:00 +0900

    :up: add app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import oseti
+
+app = Flask(__name__)
+
+@app.route('/api/score', methods=['GET'])
+def calculateScore():
+	sentence = request.args.get('q')
+	print('Sentence', sentence)
+	analyzer = oseti.Analyzer()
+	score_list = analyzer.analyze(sentence)
+	score = sum(score_list)/len(score_list)
+	print('Score:', score)
+
+	return(jsonify(score=score))
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80)
